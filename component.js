1d102ef3ffb03e377fbd9f604344b217971bf500
marsanla <marcos.sanz@13genius.com>
2014-11-02 23:31:41 +0100

    Update

--- component.js
+++ component.js
@@ -22,6 +22,38 @@ module.exports = {
 				res.end();
 			};
 
+			res.redirect = function redirect(url) {
+				var address = url;
+				var body;
+				var status = 302;
+
+				// allow status / url
+				if (arguments.length === 2) {
+					if (typeof arguments[0] === 'number') {
+						status = arguments[0];
+						address = arguments[1];
+					} else {
+						deprecate('res.redirect(ur, status): Use res.redirect(status, url) instead');
+						status = arguments[1];
+					}
+				}
+
+				// Set location header
+				if ('back' === address) address = res.get('Referrer') || '/';
+
+				// Respond
+				res.set('Location', address);
+
+				// Support text/{plain,html} by default
+				body = res.format(status + '. Redirecting to ' + encodeURI(url));
+
+				// Respond
+				res.status(status);
+				res.set('Content-Length', Buffer.byteLength(body));
+
+				res.send(body);
+			};
+
 			next();
 		});
 	}
